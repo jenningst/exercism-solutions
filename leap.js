715b7fdd2999ab7e32a22702df21803c36d2d703
Troy Jennings <troy.c.jennings@gmail.com>
2018-12-06 16:55:26 +0100

    adding refactored code w/ no ternary

--- leap.js
+++ leap.js
@@ -13,13 +13,13 @@
 
 // INTERMEDIATE SOLUTION (REFACTORED)
 export const isLeap = (year) => {
-    return year % 100 === 0 && year % 400 !== 0
-        ? false : year % 4 === 0
+    return (year % 4 === 0 && year % 100 !== 0)
+        || (year % 4 === 0 && year % 100 === 0 && year % 400 === 0)
 };
 
 // LOCAL TESTS
-// console.log(isLeap(2015)); // should return false
-// console.log(isLeap(2016)); // should return true
-// console.log(isLeap(2100)); // should return false
-// console.log(isLeap(2000)); // should return true
-// console.log(isLeap(1800)); // should return false
+console.log(isLeap(2015)); // should return false
+console.log(isLeap(2016)); // should return true
+console.log(isLeap(2100)); // should return false
+console.log(isLeap(2000)); // should return true
+console.log(isLeap(1800)); // should return false
