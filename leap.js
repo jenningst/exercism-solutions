c4f983be64949cd47230a464f1d3b2e5727ba49e
Troy Jennings <troy.c.jennings@gmail.com>
2018-12-06 17:03:15 +0100

    final

--- leap.js
+++ leap.js
@@ -14,12 +14,12 @@
 // INTERMEDIATE SOLUTION (REFACTORED)
 export const isLeap = (year) => {
     return (year % 4 === 0 && year % 100 !== 0)
-        || (year % 4 === 0 && year % 100 === 0 && year % 400 === 0)
+        || (year % 400 === 0)
 };
 
 // LOCAL TESTS
-console.log(isLeap(2015)); // should return false
-console.log(isLeap(2016)); // should return true
-console.log(isLeap(2100)); // should return false
-console.log(isLeap(2000)); // should return true
-console.log(isLeap(1800)); // should return false
+// console.log(isLeap(2015)); // should return false
+// console.log(isLeap(2016)); // should return true
+// console.log(isLeap(2100)); // should return false
+// console.log(isLeap(2000)); // should return true
+// console.log(isLeap(1800)); // should return false
